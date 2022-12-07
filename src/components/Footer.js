f7629b9844074e6d2fe6f4f3841a079d3a4bf5b4
Brad Fike <brad@fikeweb.com>
2022-12-07 13:40:19 -0600

    Header and Footer Links: wk2 ex4

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable react/react-in-jsx-scope */
-import {Container, Row, Col} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -8,7 +9,21 @@ const Footer = () => {
       <Container>
         <Row>
           <Col xs={{size: 4, offset: 1}} sm='2'>
-            <h5>this will be site navigation!</h5>
+            <h5>Links</h5>
+            <ul className='list-unstyled'>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/directory'>Directory</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact</Link>
+              </li>
+            </ul>
           </Col>
           <Col xs='6' sm='3' className='text-center'>
 
