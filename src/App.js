a707f79a3ed784f3cca65a04056b1c7b1d8e4b95
Brad Fike <brad@fikeweb.com>
2022-12-10 16:12:53 -0600

    week 2 workshop: About Page and Partner list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           element={<CampsiteDetailPage />}
         />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='about' element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
