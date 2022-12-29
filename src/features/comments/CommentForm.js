fde8afacb984bb403c02d91dfa64c60df9bfb1b6
Brad Fike <brad@fikeweb.com>
2022-12-29 07:24:03 -0600

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Button, Modal, ModalHeader, FormGroup, Label } from 'reactstrap';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { validateCommentForm } from '../../utils/validateCommentForm';
+import { addComment } from './commentsSlice';
 
 const CommentForm = ({ campsiteId }) => {
         const [modalOpen, setModalOpen] = useState(false);
@@ -12,15 +14,17 @@ const CommentForm = ({ campsiteId }) => {
                 commentText: '',
                 campsiteId: parseInt(campsiteId),
         };
-
+        const dispatch = useDispatch();
         const handleSubmit = (values) => {
                 const comment = {
                         campsiteId: parseInt(values.campsiteId),
                         rating: values.rating,
                         author: values.author,
                         text: values.commentText,
+                        date: new Date(Date.now()).toISOString(),
                 };
                 console.log(comment);
+                dispatch(addComment(comment));
                 setModalOpen(false);
         };
 
